1305b3956b52213a6a063af7a1365bdb2fa8d27e
Tommy Seery <Tommy.Seery@infonetica.net>
2026-07-26 15:13:51 +0100

    fix(apphost): keep service name config key package-safe [skip-e2e]

--- src/Concertable.Customer.Web/Program.cs
+++ src/Concertable.Customer.Web/Program.cs
@@ -38,7 +38,6 @@ using Concertable.ServiceDefaults;
 using Concertable.DataAccess.Infrastructure.Data;
 using Concertable.Messaging.Application.Extensions;
 using Concertable.Messaging.AzureServiceBus.Extensions;
-using Concertable.Messaging.AzureServiceBus.Options;
 using Concertable.Kernel.Extensions;
 using Concertable.Shared.Notification.Infrastructure.Hubs;
 using Concertable.Shared.Notification.Infrastructure.Extensions;
@@ -96,9 +95,9 @@ services.AddAzureServiceBusTransport(
         opts.ConnectionString = builder.Configuration.GetConnectionString("asb")
             ?? (builder.Environment.IsEnvironment("Testing") ? null!
                 : throw new InvalidOperationException("Connection string 'asb' is required."));
-        opts.ServiceName = builder.Configuration[AzureServiceBusOptions.ServiceNameConfigKey]
+        opts.ServiceName = builder.Configuration["ServiceBus:ServiceName"]
             ?? (builder.Environment.IsEnvironment("Testing") ? "concertable-customer"
-                : throw new InvalidOperationException($"Configuration '{AzureServiceBusOptions.ServiceNameConfigKey}' is required."));
+                : throw new InvalidOperationException("Configuration 'ServiceBus:ServiceName' is required."));
     },
     reg =>
     {
