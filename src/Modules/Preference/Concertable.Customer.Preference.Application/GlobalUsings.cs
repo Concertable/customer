a4666b2216ceacfed3f0077ebf0f628741dd240c
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-09 23:54:00 +0100

    Remove stale Customer Kernel references

--- src/Modules/Preference/Concertable.Customer.Preference.Application/GlobalUsings.cs
+++ src/Modules/Preference/Concertable.Customer.Preference.Application/GlobalUsings.cs
@@ -1,6 +1,4 @@
-global using Concertable.Kernel;
 global using Reunion;
-global using Concertable.Kernel.ValueObjects;
 global using Concertable.Contracts;
 global using Concertable.Contracts.Enums;
 global using Concertable.DataAccess.Application;
