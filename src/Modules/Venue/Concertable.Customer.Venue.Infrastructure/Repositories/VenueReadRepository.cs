d45278ef2ff570f3def466943aa94e9e34afecec
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-13 17:46:24 +0100

    Compose repository read and write facets

--- src/Modules/Venue/Concertable.Customer.Venue.Infrastructure/Repositories/VenueReadRepository.cs
+++ src/Modules/Venue/Concertable.Customer.Venue.Infrastructure/Repositories/VenueReadRepository.cs
@@ -1,17 +1,18 @@
 using Concertable.Customer.Venue.Application.DTOs;
 using Concertable.Customer.Venue.Domain.Entities;
 using Concertable.Customer.Venue.Infrastructure.Mappers;
-using Concertable.Customer.DataAccess.Infrastructure;
+using Concertable.DataAccess.Application;
+using Concertable.DataAccess.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace Concertable.Customer.Venue.Infrastructure.Repositories;
 
-internal sealed class VenueReadRepository : ReadRepository<VenueEntity>, IVenueReadRepository
+internal sealed class VenueReadRepository : ReadRepository<VenueEntity, int>, IVenueReadRepository
 {
     public VenueReadRepository(IReadDbContext context) : base(context) { }
 
     public Task<VenueDetails?> GetDetailsByIdAsync(int venueId) =>
-        context.Query<VenueEntity>()
+        base.context.Query<VenueEntity>()
             .Where(v => v.Id == venueId)
             .ToDetails()
             .FirstOrDefaultAsync();
