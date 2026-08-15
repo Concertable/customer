94a24f2694f4e8200dce15bcde07b8c89eb4e3ad
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-15 09:29:33 +0100

    test(customer): remove stale review location assertion

--- src/Modules/Review/Tests/Concertable.Customer.Review.IntegrationTests/ReviewApiTests.cs
+++ src/Modules/Review/Tests/Concertable.Customer.Review.IntegrationTests/ReviewApiTests.cs
@@ -257,7 +257,6 @@ public sealed class ReviewApiTests : IAsyncLifetime
         Assert.NotNull(review);
         Assert.Equal(4, review.Stars);
         Assert.Equal("Great concert", review.Details);
-        Assert.Equal($"/api/concerts/{concert.Id}/reviews", response.Headers.Location?.OriginalString);
     }
 
     #endregion
