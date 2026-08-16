65461ae1431a22970b942ee2ebf092e8298af517
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-16 18:35:20 +0100

    refactor(data): align standard repository contracts

--- src/Modules/User/Concertable.Customer.User.Application/Interfaces/IUserRepository.cs
+++ src/Modules/User/Concertable.Customer.User.Application/Interfaces/IUserRepository.cs
@@ -1,11 +1,9 @@
+using Concertable.DataAccess.Application;
 using Concertable.Customer.User.Domain.Entities;
 
 namespace Concertable.Customer.User.Application.Interfaces;
 
-internal interface IUserRepository
+internal interface IUserRepository : IRepository<UserEntity, Guid>
 {
-    Task<UserEntity?> GetByIdAsync(Guid id);
     Task<IReadOnlyList<UserEntity>> GetByIdsAsync(IEnumerable<Guid> ids);
-    void Update(UserEntity entity);
-    Task SaveChangesAsync();
 }
