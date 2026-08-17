122d989aa04ff22d19a811e4d944d1d554aef405
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-17 07:36:34 +0100

    refactor(data-access): contract repository permission hierarchy

--- src/Modules/Ticket/Concertable.Customer.Ticket.Infrastructure/Repositories/Repository.cs
+++
@@ -1,8 +0,0 @@
-using Concertable.Customer.Ticket.Infrastructure.Data;
-using Concertable.Kernel;
-
-namespace Concertable.Customer.Ticket.Infrastructure.Repositories;
-
-internal abstract class WriteRepository<TEntity>(TicketDbContext context)
-    : WriteRepository<TEntity, TicketDbContext>(context)
-    where TEntity : class;
