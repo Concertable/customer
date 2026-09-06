909c8b0c87016f7aba6af42fe303e5f8c9ed9f84
ThomasSeery <tommyboy4050@outlook.com>
2026-09-06 01:06:19 +0100

    Move Auth SPA mappings to service owners

--- src/Concertable.Customer.Hosting/CustomerLocalSpaSurfaces.cs
+++ src/Concertable.Customer.Hosting/CustomerLocalSpaSurfaces.cs
@@ -2,7 +2,10 @@ namespace Concertable.Customer.Hosting;
 
 public static class CustomerLocalSpaSurfaces
 {
-    public static SpaSurface Customer { get; } = new("customer", 5174, "Customer");
+    public static SpaSurface Customer { get; } = new("customer", 5174);
 
     public static IReadOnlyList<SpaSurface> All { get; } = Array.AsReadOnly([Customer]);
+
+    public static IReadOnlyList<(SpaSurface Surface, string ClientName)> AuthClients { get; } =
+        Array.AsReadOnly<(SpaSurface Surface, string ClientName)>([(Customer, "Customer")]);
 }
