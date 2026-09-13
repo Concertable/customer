22363a4a1cb4ebcfc02f6a53f0e2c4290ff94dfe
Tommy Seery <Tommy.Seery@infonetica.net>
2026-09-13 19:49:11 +0100

    fix(web): configure the customer API client

--- app/web/src/lib/customerClient.ts
+++ app/web/src/lib/customerClient.ts
@@ -1,6 +1,6 @@
 import { customerClient } from "@concertable/customer/lib/customerClient";
 import { configureWebClient } from "@concertable/web/lib/configureWebClient";
 
-configureWebClient(customerClient, import.meta.env.VITE_CUSTOMER_API_URL);
+configureWebClient(customerClient, import.meta.env.VITE_API_URL);
 
 export { customerClient };
