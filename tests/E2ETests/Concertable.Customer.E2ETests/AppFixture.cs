e25089d9454e5f07860e0ea0594174c9dfcc3ba4
Tommy Seery <Tommy.Seery@infonetica.net>
2026-09-13 19:34:57 +0100

    fix(e2e): use HTTP-compatible Auth image

--- tests/E2ETests/Concertable.Customer.E2ETests/AppFixture.cs
+++ tests/E2ETests/Concertable.Customer.E2ETests/AppFixture.cs
@@ -18,7 +18,7 @@ namespace Concertable.Customer.E2ETests;
 
 public sealed class AppFixture : IAsyncLifetime
 {
-    private const string AuthE2EDigest = "sha256:b92399c92ddff69b7916935b0b7b31afb52460b3151997f60d32124c9ff70c2c";
+    private const string AuthE2EDigest = "sha256:e228e89af3fa51f1dd7995d33e109bb3a9c2bf2ab3bed98df28e3ef7bac28251";
     private const string PaymentE2EWebDigest = "sha256:df33de77f2d01558f9ffb3b0d1cc68ddcd26e41f6d54f65045caf3e466b4a775";
     private const string PaymentE2EWorkersDigest = "sha256:4385c505153cca1df16983864b0c99807537b37f8aea801d434092cce47c87c8";
 
